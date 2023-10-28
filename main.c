f3cc71525ba8c10267012a7281fb61065f9ffdca
ransel117 <ransel117@protonmail.com>
2023-10-28 18:23:20 +0200

    removed string_VkResult

--- main.c
+++ main.c
@@ -11,117 +11,15 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
 
-static inline const char* string_VkResult(VkResult input_value) {
-    switch (input_value) {
-        case VK_SUCCESS:
-            return "VK_SUCCESS";
-        case VK_NOT_READY:
-            return "VK_NOT_READY";
-        case VK_TIMEOUT:
-            return "VK_TIMEOUT";
-        case VK_EVENT_SET:
-            return "VK_EVENT_SET";
-        case VK_EVENT_RESET:
-            return "VK_EVENT_RESET";
-        case VK_INCOMPLETE:
-            return "VK_INCOMPLETE";
-        case VK_ERROR_OUT_OF_HOST_MEMORY:
-            return "VK_ERROR_OUT_OF_HOST_MEMORY";
-        case VK_ERROR_OUT_OF_DEVICE_MEMORY:
-            return "VK_ERROR_OUT_OF_DEVICE_MEMORY";
-        case VK_ERROR_INITIALIZATION_FAILED:
-            return "VK_ERROR_INITIALIZATION_FAILED";
-        case VK_ERROR_DEVICE_LOST:
-            return "VK_ERROR_DEVICE_LOST";
-        case VK_ERROR_MEMORY_MAP_FAILED:
-            return "VK_ERROR_MEMORY_MAP_FAILED";
-        case VK_ERROR_LAYER_NOT_PRESENT:
-            return "VK_ERROR_LAYER_NOT_PRESENT";
-        case VK_ERROR_EXTENSION_NOT_PRESENT:
-            return "VK_ERROR_EXTENSION_NOT_PRESENT";
-        case VK_ERROR_FEATURE_NOT_PRESENT:
-            return "VK_ERROR_FEATURE_NOT_PRESENT";
-        case VK_ERROR_INCOMPATIBLE_DRIVER:
-            return "VK_ERROR_INCOMPATIBLE_DRIVER";
-        case VK_ERROR_TOO_MANY_OBJECTS:
-            return "VK_ERROR_TOO_MANY_OBJECTS";
-        case VK_ERROR_FORMAT_NOT_SUPPORTED:
-            return "VK_ERROR_FORMAT_NOT_SUPPORTED";
-        case VK_ERROR_FRAGMENTED_POOL:
-            return "VK_ERROR_FRAGMENTED_POOL";
-        case VK_ERROR_UNKNOWN:
-            return "VK_ERROR_UNKNOWN";
-        case VK_ERROR_OUT_OF_POOL_MEMORY:
-            return "VK_ERROR_OUT_OF_POOL_MEMORY";
-        case VK_ERROR_INVALID_EXTERNAL_HANDLE:
-            return "VK_ERROR_INVALID_EXTERNAL_HANDLE";
-        case VK_ERROR_FRAGMENTATION:
-            return "VK_ERROR_FRAGMENTATION";
-        case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS:
-            return "VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS";
-        case VK_PIPELINE_COMPILE_REQUIRED:
-            return "VK_PIPELINE_COMPILE_REQUIRED";
-        case VK_ERROR_SURFACE_LOST_KHR:
-            return "VK_ERROR_SURFACE_LOST_KHR";
-        case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
-            return "VK_ERROR_NATIVE_WINDOW_IN_USE_KHR";
-        case VK_SUBOPTIMAL_KHR:
-            return "VK_SUBOPTIMAL_KHR";
-        case VK_ERROR_OUT_OF_DATE_KHR:
-            return "VK_ERROR_OUT_OF_DATE_KHR";
-        case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
-            return "VK_ERROR_INCOMPATIBLE_DISPLAY_KHR";
-        case VK_ERROR_VALIDATION_FAILED_EXT:
-            return "VK_ERROR_VALIDATION_FAILED_EXT";
-        case VK_ERROR_INVALID_SHADER_NV:
-            return "VK_ERROR_INVALID_SHADER_NV";
-        case VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR:
-            return "VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR";
-        case VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR:
-            return "VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR";
-        case VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR:
-            return "VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR";
-        case VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR:
-            return "VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR";
-        case VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR:
-            return "VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR";
-        case VK_ERROR_VIDEO_STD_VERSION_NOT_SUPPORTED_KHR:
-            return "VK_ERROR_VIDEO_STD_VERSION_NOT_SUPPORTED_KHR";
-        case VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT:
-            return "VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT";
-        case VK_ERROR_NOT_PERMITTED_KHR:
-            return "VK_ERROR_NOT_PERMITTED_KHR";
-        case VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT:
-            return "VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT";
-        case VK_THREAD_IDLE_KHR:
-            return "VK_THREAD_IDLE_KHR";
-        case VK_THREAD_DONE_KHR:
-            return "VK_THREAD_DONE_KHR";
-        case VK_OPERATION_DEFERRED_KHR:
-            return "VK_OPERATION_DEFERRED_KHR";
-        case VK_OPERATION_NOT_DEFERRED_KHR:
-            return "VK_OPERATION_NOT_DEFERRED_KHR";
-#ifdef VK_ENABLE_BETA_EXTENSIONS
-        case VK_ERROR_INVALID_VIDEO_STD_PARAMETERS_KHR:
-            return "VK_ERROR_INVALID_VIDEO_STD_PARAMETERS_KHR";
-#endif //VK_ENABLE_BETA_EXTENSIONS
-        case VK_ERROR_COMPRESSION_EXHAUSTED_EXT:
-            return "VK_ERROR_COMPRESSION_EXHAUSTED_EXT";
-        case VK_ERROR_INCOMPATIBLE_SHADER_BINARY_EXT:
-            return "VK_ERROR_INCOMPATIBLE_SHADER_BINARY_EXT";
-        default:
-            return "Unhandled VkResult";
-    }
-}
-
 typedef struct app_info_t app_info;
 
 struct app_info_t {
     SDL_Window *window;
 
     VkInstance instance;
-    bool use_validation_layer;
+    VkSurfaceKHR surface;
 
+    bool use_validation_layer;
     VkDebugUtilsMessengerEXT dbg_msger;
 
     char *title;
@@ -195,7 +93,7 @@ void app_init_vulkan(app_info *ai) {
     }
 
     const char* required_extensions[extensions_count + 1];
-    if (!SDL_Vulkan_GetInstanceExtensions(ai->window, &extensions_count, (const char**)required_extensions)) {
+    if (!SDL_Vulkan_GetInstanceExtensions(ai->window, &extensions_count, required_extensions)) {
         APP_ERR_EXIT("could not get required extensions: %s\n", SDL_GetError());
     }
 
@@ -235,17 +133,21 @@ void app_init_vulkan(app_info *ai) {
     VkResult result;
     result = vkCreateInstance(&vk_create_info, NULL, &ai->instance);
 
-    if (result != VK_SUCCESS) {
-        APP_ERR_EXIT("could not create vulkan instance: result: %d -> %s\n", result, string_VkResult(result));
+    if (result < VK_SUCCESS) {
+        APP_ERR_EXIT("could not create vulkan instance: result: %d\n", result);
     }
     if (ai->use_validation_layer) {
         app_populate_dbg_msger(&vk_dbg_info);
 
         result = app_create_dbg_utils_msger(ai->instance, &vk_dbg_info, NULL, &ai->dbg_msger);
 
-        if (result != VK_SUCCESS) {
-            APP_ERR_EXIT("could setup debug msger: result: %d -> %s\n", result, string_VkResult(result));
+        if (result < VK_SUCCESS) {
+            APP_ERR_EXIT("could not setup debug msger: result: %d\n", result);
         }
+    }
+
+    if (!SDL_Vulkan_CreateSurface(ai->window, ai->instance, &ai->surface)) {
+        APP_ERR_EXIT("could not create surface: %s\n", SDL_GetError());
     }
 }
 
@@ -258,6 +160,7 @@ void app_quit(app_info *ai) {
     if (ai->use_validation_layer) {
         app_destroy_dbg_utils_msger(ai->instance, ai->dbg_msger, NULL);
     }
+    vkDestroySurfaceKHR(ai->instance, ai->surface, NULL);
     vkDestroyInstance(ai->instance, NULL);
 
     SDL_DestroyWindow(ai->window);
